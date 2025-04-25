724e6d7f2e36205984d290df84de304f96ec47dc
Nelson-5553 <andresnelson2004@hotmail.com>
2025-04-25 11:36:03 -0500

    add adapter from vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,23 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   env: {
     schema: {
       API_HOST_HOME: envField.string({ context: "server", access: "secret" }),
       API_POST: envField.string({ context: "server", access: "public" }),
       API_USER: envField.string({ context: "server", access: "public" }),
     }
-  }
+  },
+
+  adapter: vercel()
 });
